e39e57e069f7dd6f488114d702ac463d571c9f8e
Aybek <turk.883ar@gmail.com>
2023-04-15 04:08:56 -0700

    update: transaction entities

--- model/transaction.go
+++ model/transaction.go
@@ -3,18 +3,18 @@ package model
 import "time"
 
 type Transaction struct {
-	ID        uint    `json:"id"`
-	UserID    uint    `json:"userID"`
-	Card      string  `json:"card"`
-	Amount    float64 `json:"amount"`
-	Items     []*TransactionItem
-	CreatedAt time.Time `json:"createdAt"`
+	ID         uint    `json:"id"`
+	AccountID  uint    `json:"accountID"`
+	CardNumber string  `json:"cardNumber"`
+	Amount     float64 `json:"amount"`
+	Items      []*TransactionItem
+	CreatedAt  time.Time `json:"createdAt"`
 }
 
 type TransactionItem struct {
-	ID          uint `json:"ID"`
-	Transaction *Transaction
-	ProductName string  `json:"ProductName"`
-	Price       float64 `json:"Price"`
-	Quantity    int     `json:"quantity"`
+	ID            uint    `json:"ID"`
+	TransactionID uint    `json:"transactionID"`
+	ProductName   string  `json:"ProductName"`
+	Price         float64 `json:"Price"`
+	Quantity      int     `json:"quantity"`
 }
