9e3faec4920718f8a4135d5ab767afc419c40704
Aybek <turk.883ar@gmail.com>
2023-04-15 02:54:30 -0700

    add: transport

---
+++ transport/http/router.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+	"net/http"
+)
+
+func (s *Server) SetUpRoute() {
+	v1 := s.App.Group("/api/v1")
+
+	s.App.GET("/swagger/*", echoSwagger.EchoWrapHandler())
+	s.App.GET("/live", func(e echo.Context) error {
+		return e.NoContent(http.StatusOK)
+	})
+
+	user := v1.Group("/accounts")
+	user.POST("", s.handler.CreateUserAccount)
+	user.PATCH("", s.handler.UpdateUserAccount)
+	user.DELETE("/:id", s.handler.DeleteUserAccount)
+
+	// v1.POST("/transactions")
+}
